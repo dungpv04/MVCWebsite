3ecb67fb2142477fdc9505455418136a1f6e6740
vandu <vandu@DESKTOP-CKTBGDM>
2023-09-21 20:37:44 +0700

    Author View Update

--- Models/BookViewModel.cs
+++ Models/BookViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,8 @@ namespace DemoMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Content { get; set; }
+        [DisplayName("Tên tác giả")]
+        [Required]
         public int? AuthorId { get; set; }
         public string AuthorName { get; set; }
         public IEnumerable<SelectListItem> Authors { get; set; }
